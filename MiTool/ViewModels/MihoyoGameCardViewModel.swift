f0274608ccf261b84149fbe5aba8be5e95b47bae
CainLuo <350116542@qq.com>
2024-03-03 23:43:25 +0800

    Add new user save or upgrade data base logic

--- MiTool/ViewModels/MihoyoGameCardViewModel.swift
+++ MiTool/ViewModels/MihoyoGameCardViewModel.swift
@@ -13,11 +13,11 @@ class MihoyoGameCardViewModel: ObservableObject {
     private let api = MockApi.shared
     private let manager = SQLManager.shared
     
-    @Published var gameRecord: [MihoyoGameRecordList] = []
+    @Published var gameRecord: [MihoyoGameCardsList] = []
     @Published var language: String = ""
 
     func getMihoyoGameCard(_ uid: String) {
-        guard let gameRecord = api.getMihoyoGameRecord().data?.list,
+        guard let gameRecord = api.getMihoyoGameCards().data?.list,
               let gameCardJSON = gameRecord.toJSONString() else {
             return
         }
--- MiTool/ViewModels/MihoyoUserListViewModel.swift
+++ MiTool/ViewModels/MihoyoUserListViewModel.swift
@@ -28,14 +28,64 @@ class MihoyoUserListViewModel: ObservableObject {
             uid: uid,
             cookie: cookie
         )
+
+        let userInfo = api.getMihoyoUserInfo()
         
-        manager.addMihoyoUser(user) { success, error in
-            guard success else {
-                print("save failed: \(error?.localizedDescription ?? "")")
-                return
+        guard let uid = userInfo.data?.userInfo?.uid else {
+            return
+        }
+        
+        if manager.getMihoyoUserList().isEmpty {
+            manager.addMihoyoUser(user) { success, error in
+                guard success else {
+                    debugPrint("Save failed: \(error?.localizedDescription ?? "")")
+                    return
+                }
+                self.saveUserSuccess = success
+                self.getMihoyoUserList()
             }
-            self.saveUserSuccess = success
-            self.getMihoyoUserList()
+        } else {
+            manager.upgradeMihoyoUser(uuid: uid, user) { success, error in
+                guard success else {
+                    debugPrint("Upgrade failed: \(error?.localizedDescription ?? "")")
+                    return
+                }
+                self.saveUserSuccess = success
+                self.getMihoyoUserList()
+            }
+        }
+        saveMihoyoGameCards(uid: uid)
+    }
+    
+    func saveMihoyoGameCards(uid: String) {
+        let gameCards = api.getMihoyoGameCards()
+
+        guard let gameCards = gameCards.data?.list else {
+            return
+        }
+        
+        if manager.getAllMihoyoGameCards(uuid: uid).isEmpty {
+            manager.addMihoyoGameCards(
+                uuid: uid,
+                model: gameCards
+            ) { success, error in
+                guard success else {
+                    debugPrint("Save failed: \(error?.localizedDescription ?? "")")
+                    return
+                }
+                debugPrint("Save mihoyo game cards success")
+            }
+        } else {
+            manager.upgradeMihoyoGameCards(
+                uuid: uid,
+                model: gameCards
+            ) { success, error in
+                guard success else {
+                    debugPrint("Upgrade failed: \(error?.localizedDescription ?? "")")
+                    return
+                }
+                debugPrint("Upgrade mihoyo game cards success")
+            }
         }
     }
 }
