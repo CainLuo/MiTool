ff7ef9d4598c2731c87d31a90fed132dd8adc9ce
CainLuo <350116542@qq.com>
2024-03-03 23:42:25 +0800

    Change file name and add new files

---
+++ MiTool/Models/WidgetSectionModel.swift
@@ -0,0 +1,15 @@
+//
+//  WidgetSectionModel.swift
+//  MiTool
+//
+//  Created by Cain Luo on 2024/3/2.
+//
+
+import Foundation
+
+struct WidgetSectionModel {
+    var uid: String
+    var title: String
+    var starRailDailyNode: StarRailWeightDataModel
+    var genshinDailyNode: GenshinImpactWidgetData
+}
