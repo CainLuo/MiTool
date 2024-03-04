a62373e3b9b5e43dae26f63291d2b0e3933c85ac
CainLuo <cain.jh.luo@gtomato.com>
2024-03-04 15:36:17 +0800

    Add swift-openapi-generator package

---
+++ MiToolAPIClient/MiToolAPIClient.h
@@ -0,0 +1,18 @@
+//
+//  MiToolAPIClient.h
+//  MiToolAPIClient
+//
+//  Created by Cain on 2024/3/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MiToolAPIClient.
+FOUNDATION_EXPORT double MiToolAPIClientVersionNumber;
+
+//! Project version string for MiToolAPIClient.
+FOUNDATION_EXPORT const unsigned char MiToolAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiToolAPIClient/PublicHeader.h>
+
+
